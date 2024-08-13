5f927a6f1308f8e9e0e20d3866d619aa0400802a
Motin <motin@motin.eu>
2024-08-13 10:01:46 +0300

    Browser based load test tweaks

--- load-testing/browser-based.js
+++ load-testing/browser-based.js
@@ -6,7 +6,7 @@ export const options = {
     ui: {
       executor: 'constant-vus',
       vus: 25,
-      duration: '30s',
+      duration: '120s',
       options: {
         browser: {
           type: 'chromium',
@@ -24,30 +24,36 @@ export default async function() {
 
   try {
 
-    //const baseUrl = 'https://www.gapminder.org/tools/'
-    //const baseUrl = 'http://static.gapminderdev.org/only-ddfcsv/'
-    const baseUrl = 'http://static.gapminderdev.org/small-waffle/'
-    // const baseUrl = 'http://static.gapminderdev.org/big-waffle/'
-    //const query = '#$ui$chart$inpercent:true;;&model$markers$pyramid$data$space@=geo&=year&=age&=gender;&filter$dimensions$geo$/$or@$geo$/$in@=world&=chn&=rus;;;;;;;;&encoding$side$data$constant:null&concept=gender;;;;;;&chart-type=popbyage&url=v2';
-    const query = '#$model$markers$bubble$data$space@=geo&=gender&=time;&filter$dimensions$geo:null;;;&encoding$size$data$space@=geo&=time;;;&y$data$concept=literacy_rate_adult&source=sg;&scale$domain:null&zoomed:null&type:null;;&x$data$space@=geo&=time;;;&label$data$source=sg;;&frame$value=2011;;;;;&chart-type=bubbles&url=v2';
-    await page.goto(`${baseUrl}${query}`);
+    const toolsPageBaseUrl = 'https://www.gapminder.org/tools/'
+    const onlyDdfCsvBaseUrl = 'http://static.gapminderdev.org/only-ddfcsv/'
+    const smallWaffleBaseUrl = 'http://static.gapminderdev.org/small-waffle/'
+    const smallWaffleNoProxyBaseUrl = 'http://static.gapminderdev.org/small-waffle-noproxy/'
+    // const bigWaffleBaseUrl = 'http://static.gapminderdev.org/big-waffle/' // Non-proxied big waffle
+
+    const bubblesTest = {
+      'query': '#$model$markers$bubble$data$space@=geo&=gender&=time;&filter$dimensions$geo:null;;;&encoding$size$data$space@=geo&=time;;;&y$data$concept=literacy_rate_adult&source=sg;&scale$domain:null&zoomed:null&type:null;;&x$data$space@=geo&=time;;;&label$data$source=sg;;&frame$value=2011;;;;;&chart-type=bubbles&url=v2',
+      'selector': '#vzb-bc-bubble-1¬idn-c0-0-0',
+    }
+    const agesTest = {
+      'query': '#$ui$chart$inpercent:true;;&model$markers$pyramid$data$space@=geo&=year&=age&=gender;&filter$dimensions$geo$/$or@$geo$/$in@=world&=chn&=rus;;;;;;;;&encoding$side$data$constant:null&concept=gender;;;;;;&chart-type=popbyage&url=v2',
+      'selector': '.vzb-bc-stack.vzb-bc-stack-1',
+    }
+
+    const baseUrl = toolsPageBaseUrl
+    const test = agesTest
+
+    await page.goto(`${baseUrl}${test.query}`);
 
     await page.waitForNavigation();
 
     page.screenshot({ path: 'screenshot-after-nav.png' });
 
-    const selector = '#vzb-bc-bubble-1¬idn-c0-0-0'
-    // const selector = '.vzb-bc-stack.vzb-bc-stack-1'
+    const selector = test.selector
 
     await page.waitForSelector(selector, { timeout: 20000 });
 
     page.screenshot({ path: 'screenshot-after-wait.png' });
 
-    const isSelectorVisible = await page.locator(selector).isVisible();
-
-    check(page, {
-      'selector is visible': (p) => isSelectorVisible,
-    });
   } finally {
     await page.close();
   }
